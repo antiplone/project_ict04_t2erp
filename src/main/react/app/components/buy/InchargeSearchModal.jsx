d530e6e7f727bef4ae4d8507bd4d964030e38138
jiyeon <hjybd1004@naver.com>
2025-04-11 18:36:23 +0900

    buy select, insert, delete complete

--- src/main/react/app/components/buy/InchargeSearchModal.jsx
+++ src/main/react/app/components/buy/InchargeSearchModal.jsx
@@ -1,16 +1,19 @@
+// 담당자 검색 모달
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
 import { Button, Table, Modal, Checkbox } from "rsuite";
+import "../../styles/buy.css";
 
 const { Column, HeaderCell, Cell } = Table;
 
-const SellEmployeeSearchModal = ({ title, confirm, cancel, onInchargeSelect, handleOpen, handleColse } /* = props:속성 */) => {
+const InchargeSearchModal = ({  confirm, cancel, onInchargeSelect, handleOpen, handleColse } /* = props:속성 */) => {
 	
 	const [employeeList, setEmployeeList] = useState([]);
 	const [selectedIncharge, setSelectedIncharge] = useState(null);
 
 		// fetch()를 통해 톰캣서버에게 데이터를 요청
 		useEffect(() => {
-			fetch("http://localhost:8081/sell/searchEmployee", {
+			fetch("http://localhost:8081/buy/buyInchargeList", {
 				method: "GET"
 			})
 			.then(res => res.json())
@@ -35,31 +38,24 @@ const SellEmployeeSearchModal = ({ title, confirm, cancel, onInchargeSelect, han
 			}
 		};
 
-		// 모달이 열릴 때 선택값 초기화
-		useEffect(() => {
-			if (handleOpen) {
-				setSelectedIncharge(null);
-			}
-		}, [handleOpen]);
-
 	return (
 		<Modal open={handleOpen} onClose={handleColse} size="xs">
 			<Modal.Header>
-				<Modal.Title>{title}</Modal.Title>
+				<Modal.Title>담당자 검색</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
 			<Table
 				height={400}
-				data={employeeList}
+				data={(employeeList ?? []).filter(emp => emp !== null && emp !== undefined)}
 			>
 				<Column width={100} align="center" fixed>
 					<HeaderCell>선택</HeaderCell>
 					
-					<Cell>{(rowData) => (
+					<Cell>{(empData) => (
 						<Checkbox 
-						checked={selectedIncharge?.e_id === rowData.e_id} 
+						checked={selectedIncharge?.e_id === empData.e_id} 
                         onChange={(_, checked) => 
-							inchargeChkChange(checked, rowData)}
+							inchargeChkChange(checked, empData)}
 						/>
 						)}
 			  		</Cell>
@@ -68,17 +64,17 @@ const SellEmployeeSearchModal = ({ title, confirm, cancel, onInchargeSelect, han
 				<Column width={100} align="center" fixed>
 					<HeaderCell>사번</HeaderCell>
 					
-					<Cell>{(rowData) => rowData.e_id}</Cell>
+					<Cell>{(empData) => empData.e_id}</Cell>
 				</Column>
 
 				<Column width={150}>
 					<HeaderCell>담당자명</HeaderCell>
-					<Cell>{(rowData) => rowData.e_name}</Cell>
+					<Cell>{(empData) => empData.e_name}</Cell>
 				</Column>
 
 				<Column width={150}>
 					<HeaderCell>부서</HeaderCell>
-					<Cell>{(rowData) => rowData.d_name}</Cell>
+					<Cell>{(empData) => empData.d_name}</Cell>
 				</Column>
 	  		</Table>
 			</Modal.Body>
@@ -94,11 +90,11 @@ const SellEmployeeSearchModal = ({ title, confirm, cancel, onInchargeSelect, han
 	);
 };
 
-SellEmployeeSearchModal.defaultProps = {
+InchargeSearchModal.defaultProps = {
 	// props가 설정이 안되어있으면, 기본(default)으로 들어갑니다.
 	title: "제목을 입력해주세요.",
 	confirm: "확인",
 	cancel: "취소",
 };
 
-export default SellEmployeeSearchModal;
+export default InchargeSearchModal;

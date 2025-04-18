46d9df1e2f4d739e99b08d31f8294a5396716a08
dAZI <antiplone@gmail.com>
2025-04-18 19:57:29 +0900

    [feat] added login info.

--- src/main/react/app/components/common/HeaderMenu.jsx
+++ src/main/react/app/components/common/HeaderMenu.jsx
@@ -5,31 +5,17 @@ import settingIcon from "#images/common/setting.png";
 import logout from "#images/common/logout.png";
 import arrow from "#images/common/arrow.png";
 
+import AppConfig from "#config/AppConfig.json"
+
 import "#components/common/css/common.css";
 
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "@remix-run/react";
-import { Image, Breadcrumb, Header, Nav, Navbar } from "rsuite";
+import { Image, Breadcrumb, Header, Nav, Navbar, Loader } from "rsuite";
 
 import ToImage from "#components/res/ToImage";
-import AppConfig from "#config/AppConfig.json";
 
 const HeaderMenu = () => {
-	const fetchURL = AppConfig.fetch["mytest"];
-
-	// 로그아웃
-	const handleLogout = async () => {
-		try {
-		   await fetch(`${fetchURL.protocol}${fetchURL.url}/auth/logout`, {
-			  method: "POST",
-			  credentials: "include" // 세션 쿠키 인증 시 필요
-		   });
-		   window.location.href = "/login"; // 혹은 navigate("/login")
-		} catch (error) {
-		   alert("로그아웃에 실패했습니다.");
-		   console.error(error);
-		}
-	 };
 
 	const location = useLocation();
 	let crumbs = location.pathname.split('/');
@@ -45,12 +31,46 @@ const HeaderMenu = () => {
 
 	const [open, setOpen] = chatState.show;
 	const [placement, setPlacement] = chatState.placement;
+	const [myInfo, setMyInfo] = useState("");
 
 	useEffect(() => {
+
+		const fetchURL = AppConfig.fetch['mytest'];
+		fetch(`${fetchURL.protocol}${fetchURL.url}/hrCard/hrCardDetail/${localStorage.getItem('e_id')}`, {
+			method: "GET",
+			mode: "cors",
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		})
+			.then((res) => {
+
+				if (res.ok) {
+
+					let entity = res.json();
+					entity.then(
+						res => {
+
+							localStorage.setItem('e_name', res.e_name);
+							localStorage.setItem('e_position', res.e_position);
+							setMyInfo(localStorage.getItem('e_name') + ' ' + localStorage.getItem('e_position') + '님');
+							console.log("Promise 완료:", res);
+						}
+					);
+				}
+				else {
+					alert("로그인을 실패했습니다.");
+				}
+			})
+			.finally(() => { // 통신실패시 예외처리
+				
+			});
+
 		if (open)
 			console.log("상담시작");
 		else console.log("상담종료");
-	}, [open, placement]);
+	}, [myInfo, open, placement]);
 
 	const handleOpen = (key) => {
 		setOpen(true);
@@ -60,7 +80,7 @@ const HeaderMenu = () => {
 	const handleLogout = () => {
 		localStorage.clear();
 		console.log("세션만료.");
-		navigate("/");
+		navigate("/", { replace: true });
 	};
 
 	if (crumbs.length > 0)
@@ -68,7 +88,7 @@ const HeaderMenu = () => {
 
 	return (
 		<Header>
-			<Navbar appearance="inverse">
+			<Navbar style={{ fontSize: "large" }} appearance="inverse">
 				<Nav>
 					{/* <Nav.Item icon={<ToImage src={mainIcon} fbText="Main" width={32} height={32} />} /> */}
 					<Nav.Menu title="관리운영">
@@ -76,6 +96,8 @@ const HeaderMenu = () => {
 						<Nav.Item onSelect={() => { navigate("basic_client") }}>거래처등록</Nav.Item>
 						<Nav.Item onSelect={() => { navigate("basic_item") }}>상품등록</Nav.Item>
 						<Nav.Item onSelect={() => { navigate("hr_emp_card") }}>인사관리</Nav.Item>
+						<Nav.Item onSelect={() => { navigate("hr_department") }}>부서관리</Nav.Item>
+						<Nav.Item onSelect={() => { navigate("hr_appointment") }}>인사발령관리</Nav.Item>
 					</Nav.Menu>
 					<Nav.Menu title="근태관리">
 						<Nav.Item onSelect={() => { navigate("att-regAttItems") }}>근태항목등록</Nav.Item>
@@ -112,16 +134,16 @@ const HeaderMenu = () => {
 						<Nav.Item onSelect={() => { navigate("finance_voucher") }}>전표관리</Nav.Item>
 					</Nav.Menu>
 					<Nav.Menu title="문의">
-						<Nav.Item onClick={handleLogout}>로그아웃</Nav.Item>
 						<Nav.Item>Company</Nav.Item>
 						<Nav.Item>Team</Nav.Item>
-						<Nav.Item onClick={() => handleOpen("right")}>
+						<Nav.Item onSelect={() => handleOpen("right")}>
 							상담하기
 						</Nav.Item>
 					</Nav.Menu>
 				</Nav>
 				<Nav pullRight>
-					<Nav.Item icon={<ToImage src={logout} width={20} height={20} />} onSelect={handleLogout}>로그아웃</Nav.Item>
+					<Nav.Item style={{ fontSize: "large" }}>{myInfo !== "" ? myInfo : <Loader />}</Nav.Item>
+					<Nav.Item icon={<ToImage src={logout} width={32} height={32} />} onSelect={handleLogout}>로그아웃</Nav.Item>
 					{/*<Nav.Item icon={<ToImage src={settingIcon} width={20} height={20} />}>설정</Nav.Item>*/}
 				</Nav>
 			</Navbar>

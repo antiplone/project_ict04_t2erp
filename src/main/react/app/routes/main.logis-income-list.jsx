42abcd248dcf38b881fea0a1b321992d628681d2
Derek0910 <kkangmhj@hotmail.com>
2025-04-26 20:24:22 +0900

    Update main.logis-income-list.jsx
    Update main.logis-income-list.jsx

--- src/main/react/app/routes/main.logis-income-list.jsx
+++ src/main/react/app/routes/main.logis-income-list.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import SearchIcon from '@rsuite/icons/Search';
 import { Link } from "@remix-run/react";
 import { Button, Container, DateRangePicker, Input, InputGroup, /*Message,  Form, */ Table } from 'rsuite';
 import Appconfig from "#config/AppConfig.json";
@@ -8,8 +7,10 @@ import InchargeSearchModal from "#components/logis/InchargeSearchModal.jsx";
 import ClientSearchModal from "#components/logis/ClientSearchModal.jsx";
 import StorageSearchModal from "#components/logis/StorageSearchModal.jsx";
 import EmailFormModal from "#components/email/EmailFormModal.jsx";
-import MessageBox from '../components/common/MessageBox';
+import readingGlasses from "#images/common/readingGlasses.png";
+import MessageBox from '#components/common/MessageBox';
 import { useToast } from '#components/common/ToastProvider';// Import useToast here
+import DBChartModal from '#components/chart/DBChartModal2.jsx'; //
 
 {/* 챗봇 */}
 /*import TextClassifier from '#components/chatbot/chatbot';*/
@@ -20,6 +21,7 @@ const OrderIncomeList = () => {
     const [orderList, setOrderList] = useState([]); // 초기값을 모르므로 빈배열로 Warehousingist에 대입
     
     const [modalOpen, setModalOpen] = useState(false); // modal visibility state
+	const [isChartModalOpen, setChartModalOpen] = useState(false);
     
     // Handle modal open/close
     const handleOpenModal = () => setModalOpen(true);
@@ -114,51 +116,75 @@ const OrderIncomeList = () => {
     return (
         <div>
             <Container >
-            	<MessageBox type="success" text="금일 입고 목록" className="main_title"/>
+            	<MessageBox type="success" text="입고 관리"/>
 				<div className="inputBox">
 					<div>
-						<InputGroup className="input">
-							<InputGroup.Addon style={{ width: 80 }}>발주일자</InputGroup.Addon>
+						<InputGroup className="input_date_type">
+							<InputGroup.Addon style={{ width: 80 }} className='text_center'>발주일자</InputGroup.Addon>
 							<DateRangePicker
 								value={orderDate}
 								onChange={handleDateChange}
-								format="yyyy-MM-dd"
 								placeholder="날짜 선택"
+								format="yyyy-MM-dd"
 							/>
 						</InputGroup>
 					</div>
-					<div className="display_flex">
-						<InputGroup className="input">
-							<InputGroup.Addon style={{ width: 80 }}>담당자</InputGroup.Addon>
-							<Input value={selectedIncharge || ""} readOnly />
-							<InputGroup.Button className="cursor_pointer" tabIndex={-1} onClick={() => setInchargeModalOpen(true)} >
-								<SearchIcon />
-							</InputGroup.Button>
-						</InputGroup>
-						<Input value={selectedInchargeName || ""} readOnly style={{ width: 150 }} />
-					</div>
-					<InputGroup className="input">
-						<InputGroup.Addon style={{ width: 80 }}>거래처</InputGroup.Addon>
-						<Input value={selectedClient || ""} readOnly />
-						<InputGroup.Addon className="cursor_pointer" onClick={() => setClientModalOpen(true)} >
-							<SearchIcon />
-						</InputGroup.Addon>
-					</InputGroup>
-					<Input value={selectedClientName || ""} readOnly style={{ width: 150 }} />
+					<div className="input">
+                        <InputGroup className="inputModal">
+                            <InputGroup.Addon style={{ width: 80 }} className='text_center'>담당자</InputGroup.Addon>
+                            <Input value={selectedIncharge || ""} readOnly onClick={() => setInchargeModalOpen(true)} />
+                            <InputGroup.Button tabIndex={-1}>
+                                <img
+                                    src={readingGlasses}
+                                    alt="돋보기"
+                                    width={20}
+                                    height={20}
+                                    style={{ cur: "pointer" }}
+                                />
+                            </InputGroup.Button>
+                        </InputGroup>
+                        <Input value={selectedInchargeName || ""} readOnly className="inputModalSide" />
+                    </div>
+					 <div className="input">
+                        <InputGroup className="inputModal">
+                            <InputGroup.Addon style={{ width: 80 }} className='text_center'>거래처</InputGroup.Addon>
+                            <Input value={selectedClient || ""} readOnly onClick={() => setClientModalOpen(true)} />
+                            <InputGroup.Addon>
+                                <img
+                                    src={readingGlasses}
+                                    alt="돋보기"
+                                    width={20}
+                                    height={20}
+                                    style={{ cur: "pointer" }}
+                                />
+                            </InputGroup.Addon>
+                        </InputGroup>
+                        <Input value={selectedClientName || ""} readOnly className="inputModalSide" />
+                    </div>
 
-					<InputGroup className="input">
-						<InputGroup.Addon style={{ width: 80 }}>입고창고</InputGroup.Addon>
-						<Input value={selectedStorage || ""} readOnly />
-						<InputGroup.Addon className="cursor_pointer" onClick={() => setStorageModalOpen(true)} >
-							<SearchIcon />
-						</InputGroup.Addon>
-					</InputGroup>
-					<Input value={selectedStorageName || ""} readOnly style={{ width: 150 }} />
+					<div className="input">
+                        <InputGroup className="inputModal">
+                            <InputGroup.Addon style={{ width: 80 }} className='text_center'>입고창고</InputGroup.Addon>
+                            <Input value={selectedStorage || ""} readOnly onClick={() => setStorageModalOpen(true)} />
+                            <InputGroup.Addon>
+                                <img
+                                    src={readingGlasses}
+                                    alt="돋보기"
+                                    width={20}
+                                    height={20}
+                                    style={{ cur: "pointer" }}
+                                />
+                            </InputGroup.Addon>
+                        </InputGroup>
+                        <Input value={selectedStorageName || ""} readOnly className="inputModalSide" />
+                    </div>
 
 					<ClientSearchModal handleOpen={isClientModalOpen} handleColse={() => setClientModalOpen(false)} onClientSelect={(code, name) => { setSelectedClient(code); setSelectedClientName(name); }} />
 					<InchargeSearchModal handleOpen={isInchargeModalOpen} handleColse={() => setInchargeModalOpen(false)} onInchargeSelect={(id, name) => { setSelectedIncharge(id); setSelectedInchargeName(name); }} />
 					<StorageSearchModal handleOpen={isStorageModalOpen} handleColse={() => setStorageModalOpen(false)} onStorageSelect={(code, name) => { setSelectedStorage(code); setSelectedStorageName(name); }} />
 
+				</div>
+				<div className="buyBtnBox">
 					<Button appearance="primary" onClick={handleSearch}>
 						검색
 					</Button>
@@ -166,22 +192,22 @@ const OrderIncomeList = () => {
                 <br />
                 <Table height={400} data={orderListWithRowNum} className="text_center">
                     <Table.Column width={80} align="center" fixed>
-                        <Table.HeaderCell>번호</Table.HeaderCell>
+                        <Table.HeaderCell className='text_center'>번호</Table.HeaderCell>
                         <Table.Cell dataKey="row_num" />
                     </Table.Column>
 
                     <Table.Column width={80} align="center" fixed>
-                        <Table.HeaderCell>주문고유번호</Table.HeaderCell>
+                        <Table.HeaderCell className='text_center'>주문고유번호</Table.HeaderCell>
                         <Table.Cell dataKey="order_id" />
                     </Table.Column>
 
                     <Table.Column width={120}>
-                        <Table.HeaderCell>입고일자</Table.HeaderCell>
+                        <Table.HeaderCell className='text_center'>입고일자</Table.HeaderCell>
                         <Table.Cell dataKey="delivery_date" />
                     </Table.Column>
 
                     <Table.Column width={150}>
-                        <Table.HeaderCell>아이템 비고</Table.HeaderCell>
+                        <Table.HeaderCell className='text_center'>아이템 비고</Table.HeaderCell>
                         <Table.Cell dataKey="item_name" style={{ padding: '6px' }}>
 							{rowData => (
 								<Link to={`/main/logis-order-item-list/${rowData.order_id}`} className="btn btn-primary area_fit wide_fit">
@@ -192,7 +218,7 @@ const OrderIncomeList = () => {
                     </Table.Column>
 
                     <Table.Column width={120}>
-                        <Table.HeaderCell>발주처</Table.HeaderCell>
+                        <Table.HeaderCell className='text_center'>발주처</Table.HeaderCell>
                         <Table.Cell dataKey="client_name" />
                     </Table.Column>
 
@@ -202,7 +228,7 @@ const OrderIncomeList = () => {
                         </Table.Column> */}
 
                     <Table.Column width={160}>
-                        <Table.HeaderCell>입고창고</Table.HeaderCell>
+                        <Table.HeaderCell className='text_center'>입고창고</Table.HeaderCell>
                         <Table.Cell dataKey="storage_name" />
                     </Table.Column>
                 </Table>
@@ -214,8 +240,17 @@ const OrderIncomeList = () => {
 				</div>
 
 				{/* EmailFormModal Component */}
-				<EmailFormModal open={modalOpen} onClose={() => handleCloseModal(false)} />
-				{/*<TextClassifier />*/}
+				<div style={{ display: 'flex' }}>
+					<EmailFormModal open={modalOpen} onClose={() => handleCloseModal(false)} />
+					<Button appearance="primary" onClick={() => setChartModalOpen(true)}>
+						차트 보기
+					</Button>
+
+					<DBChartModal
+						open={isChartModalOpen}
+						onClose={() => setChartModalOpen(false)}
+					/>
+				</div>
              </Container>
         </div>
     );

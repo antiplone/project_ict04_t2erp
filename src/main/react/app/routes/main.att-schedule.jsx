be3c3bd70d7a34d79681d998a2770e6b9915a465
sujeong <xksajfod@gmail.com>
2025-04-28 19:33:42 +0900

    modify schedule

--- src/main/react/app/routes/main.att-schedule.jsx
+++ src/main/react/app/routes/main.att-schedule.jsx
@@ -1,13 +1,8 @@
 // routes/main/schedule.jsx
 /* eslint-disable react/react-in-jsx-scope */
 import { Container } from "rsuite";
-import HolidayCalendar from "#components/api/HolidayCalendar";
-import HolidayCalendar3 from "#components/api/HolidayCalendar3";
-import HolidayCalendar5 from "#components/api/HolidayCalendar5";
 import MessageBox from "#components/common/MessageBox";
 import AppConfig from "#config/AppConfig.json";
-import WeatherBox from "#components/api/WeatherBox.jsx";
-import WeatherInfo from "#components/api/WeatherInfo.jsx";
 import Calendar from "#components/api/Calendar.jsx";
 
 export function meta() {
@@ -22,11 +17,8 @@ export default function SchedulePage() {
     <Container>
       <MessageBox text="공휴일 일정" />
       <div style={{ marginTop: "20px" }}>
-        {/* <HolidayCalendar5 /> */}
-        {/* <HolidayCalendar /> */}
-        {/* <HolidayCalendar3 /> */}
+        <Calendar />
       </div>
-      <Calendar />
     </Container>
   );
 }

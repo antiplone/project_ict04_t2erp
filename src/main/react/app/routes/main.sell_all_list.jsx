61e171ea44bfe2ba5a6af37caada1465eb432962
seolhwa <film8sh@naver.com>
2025-04-29 14:46:04 +0900

    sell_all_list UPDATE

--- src/main/react/app/routes/main.sell_all_list.jsx
+++ src/main/react/app/routes/main.sell_all_list.jsx
@@ -6,8 +6,6 @@ import "#styles/sell.css";
 import AppConfig from "#config/AppConfig.json";
 import SellInvoice from '#components/sell/SellInvoice.jsx';
 import MessageBox from '../components/common/MessageBox';
-import NewsList from '#components/sell/newsList.jsx';
-
 // sell_all_list => 판매 조회 페이지
 
 const { Column, HeaderCell, Cell } = Table;

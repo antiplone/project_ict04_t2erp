99742133423bef38ef6df667c4ecb54e008ddd99
sujeong <xksajfod@gmail.com>
2025-05-05 18:37:25 +0900

    Modify retirement and Modify Scheduler

--- src/main/react/app/routes/main.att-schedule.jsx
+++ src/main/react/app/routes/main.att-schedule.jsx
@@ -5,6 +5,7 @@ import MessageBox from "#components/common/MessageBox";
 import AppConfig from "#config/AppConfig.json";
 // import Calendar from "#components/api/calendar/Calendar";
 import HolidayCalendar from "#components/api/calendar/HolidayCalendar";
+import HolidayCalendars from "#components/api/calendar/HolidayCalendars.jsx";
 
 export function meta() {
   return [
@@ -35,7 +36,8 @@ export default function SchedulePage() {
         ) : (
         // 로딩 후 달력 표시
         <div style={{ marginTop: "20px" }}>
-          <HolidayCalendar />
+          {/* <HolidayCalendar /> */}
+          <HolidayCalendars />
         </div>
       )}
     </Container>
